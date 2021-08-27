09edbdd6f5e6431d0804ef3021561708c36e75b1
Nazariy Dumanskyy <nazariy1995@gmail.com>
2021-08-26 18:04:05 -0700

    DAY 1 : color randomizer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="main">
+        <div className="bg-color">
+          <h1>Background Color: #FFFFFF</h1>
+        </div>
+        <div className="button">
+          Click me
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// Task 1: Create a state variable 'color'
+// That color you will show on the frontend
+
+// Task 2: Create an array of colors (HEX-codes)
+// use coolors.co website
+
+// Task 3: Create a function that fires off upon onClick
+// onClick is on the button
+// select a random color of the array of collars
+// Use (Math.random) for the keys;
+// and set the variable color with the value of that random color
+
+
